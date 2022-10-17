85ba3b6df5e1f9d4dabd8b1a29a509494fff67b5
jasondsouza35 <jasonaldrindsouza@gmail.com>
2022-10-17 00:33:17 -0400

    Onto PHP!

--- estimatedprice.py
+++ estimatedprice.py
@@ -3,37 +3,23 @@ import csv
 import pandas as pd
 import nums_from_string
 
-# with open('local_house_info.csv') as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     line_count = 0
-#     for row in csv_reader:
-#         if line_count == 0:
-#             print(f'Column names are {", ".join(row)}')
-#             line_count += 1
-#         else:
-#             print(f'\t{row[0]} works in the {row[1]} department.')
-#             line_count += 1
-#     print(f'Processed {line_count} lines.')
-
 df = pd.read_csv("local_house_info.csv")
 with open('local_house_info.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if len(df) == line_count:
-            break
-        sqft = nums_from_string.get_nums(df.at[line_count, 'Size'])
+    for ind in df.index:
+        sqft = nums_from_string.get_nums(df.at[ind, 'Size'])
         sqft_range = 0
         if len(sqft) > 0:
             for val in sqft:
                 sqft_range += abs(val)
             sqft_range /= len(sqft)
-        df.at[line_count, 'Size'] = sqft_range
-        line_count += 1
+        df.at[ind, 'Size'] = sqft_range
     df.to_csv("local_house_info.csv", index=False)
 
-    
-# Check if the given house size is within 20% of the given size then adds the price to a list if that is the case,
-# then averages that out
+    similar_prices = []
+    for ind in df.index:
+        if webscraper.house_sqft * 0.8 < df.at[ind, 'Size'] < webscraper.house_sqft * 1.2:
+            similar_prices.append(df.at[ind, 'Price'])
 
+    estimated_price = int(sum(similar_prices) / len(similar_prices))
+    print(estimated_price)
 
